44036e4dedc5e101877386f8b5c4d522c6c9bf8c
pazamb <pabloazamb@gmail.com>
2022-04-13 19:32:11 -0300

    repaso HOME

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -200,7 +200,13 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
-
+  var newA = [];
+  for (var i = 0 ; i < array.length ; i++) {
+    if (array[i] > 100) {
+      newA.push(array[i]);
+    }
+  }
+  return newA;
 }
 
 
@@ -212,7 +218,20 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var suma = numero;
+  var nuevoArr = [];
 
+  for (var i = 0 ; i < 10 ; i++) {
+    if (suma === i)
+        break;
+        suma = suma + 2;
+        nuevoArr.push(suma);
+  }
+
+  if (nuevoArr.length === 10) {
+    return nuevoArr;
+  }
+  return 'Se interrumpió la ejecución';
 }
 
 
@@ -223,6 +242,20 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var suma = numero;
+  var nuevoArray = [];
+
+  for (var i = 0 ; i < 10 ; i++) {
+    if (i === 5)
+      continue;
+      suma = suma + 2;
+      nuevoArray.push(suma);
+  }
+
+  if (nuevoArray.length === 9) {
+    return nuevoArray;
+  }
+  return '???';
 
 }
 
